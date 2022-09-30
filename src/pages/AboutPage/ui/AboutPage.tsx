94e982e118a14e4a8a032241d4ef4d89c72645d3
Viktor Orlyanskiy <orlyanskiy_va@mail.ru>
2022-09-30 14:49:16 +0300

    add file main.yml

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,7 +1,9 @@
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
+import { classNames } from "shared/lib";
 
 const AboutPage: FC = () => {
+    classNames("", {}, []);
     const { t } = useTranslation("about");
     return <div>{t("AboutPage")}</div>;
 };
