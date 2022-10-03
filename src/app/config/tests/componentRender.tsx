c67e7f8ea0b6e15d2bcc315690998e835d07be8e
Viktor Orlyanskiy <orlyanskiy_va@mail.ru>
2022-10-03 17:14:11 +0300

    unit 4.6

--- src/app/config/tests/componentRender.tsx
+++ src/app/config/tests/componentRender.tsx
@@ -2,7 +2,7 @@ import { render } from "@testing-library/react";
 import { ReactNode } from "react";
 import { I18nextProvider } from "react-i18next";
 import { MemoryRouter } from "react-router-dom";
-import i18nForTests from "shared/config/i18n/i18nForTest";
+import i18nForTests from "app/i18n/i18nForTest";
 
 export interface renderWithRouterOptions {
     route?: string;
